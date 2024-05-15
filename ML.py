c411f907dbeaa968ad5297ce371eecd0194fcb71
andromeda123248 <85245425+andromeda123248@users.noreply.github.com>
2024-05-15 21:19:23 +0300

    Update ML.py

--- ML.py
+++ ML.py
@@ -7,6 +7,7 @@ import pandas as pd
 import json
 import nltk
 import numpy as np
+
 nltk.download('punkt')
 nltk.download('stopwords')
 import re
@@ -41,14 +42,14 @@ def process_json(file):
 
     return df
 
+
 # Получаем путь к файлу examples.json
 file_path = 'examples.json'
 
 # Пример использования функции
 df = process_json(file_path)
 
-
-#Предобработка датафрейма
+# Предобработка датафрейма
 df['description'] = df['description'].str.lower()
 
 df['description'] = df['description'].str.replace('[{}]'.format(string.punctuation), '')
@@ -63,8 +64,6 @@ df['description'] = df['description'].apply(lambda x: [word for word in x if wor
 # Приведение слов к их изначальной форме
 morph = pymorphy2.MorphAnalyzer()
 df['description'] = df['description'].apply(lambda x: [morph.parse(word)[0].normal_form for word in x])
-
-
 
 # Создаем списки для хранения описания и жанров
 max_length = df['description'].str.len().max()
@@ -85,18 +84,17 @@ for index, row in df.iterrows():
 X = np.asarray(X, dtype="object")
 y = np.asarray(y, dtype="object")
 
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 y_train = [i[0] for i in y_train]
 
-#загрузка catboost
+# загрузка catboost
 model1 = CatBoostClassifier()
 model1 = CatBoostClassifier().load_model("catboost_model")
 
 cat_features = list(range(0, len(X[0])))
 
-print(len(X), len(cat_features),len(y))
+print(len(X), len(cat_features), len(y))
 
 train_dataset = Pool(data=X_train,
                      label=y_train,
@@ -111,18 +109,18 @@ preds_class = model1.predict(eval_dataset)
 preds_proba = model1.predict_proba(eval_dataset)
 
 preds_raw = model1.predict(eval_dataset,
-                          prediction_type='RawFormulaVal')
+                           prediction_type='RawFormulaVal')
 
-#предсказание жанров
+# предсказание жанров
 predicted_genres = model1.predict(X)
 
 acc = 0
 
 for i in range(len(preds_class)):
-  if (preds_class[i][0] in set(y_test[i])):
-    acc+=1
+    if (preds_class[i][0] in set(y_test[i])):
+        acc += 1
 
-print(acc/len(y_test))
+print(acc / len(y_test))
 
 X_array = X.tolist()
 
@@ -137,11 +135,11 @@ for index, row in df.iterrows():
 
     # Create a combined list with original structure
     combined_list = []
+    combined_list.append("'" + genres + "'")  # Добавляем жанр в формате строки
     for word in description_str.split():
         combined_list.append("'" + word + "'")  # Добавляем каждое слово в формате строки
 
     # Add genre and name at the end
-    combined_list.append("'" + genres + "'")  # Добавляем жанр в формате строки
     combined_list.append("'" + name + "'")  # Добавляем название фильма в формате строки
 
     # Convert the list back to a string with the original structure
@@ -167,9 +165,10 @@ for element in mod_descr:
 samples_X = []
 samples_y = []
 
+
 def slice(arr):
   tek_len = 0
-  len_of_slice = 20
+  len_of_slice = 30
   while(tek_len+len_of_slice < len(arr)):
     samples_X.append(arr[tek_len:tek_len+len_of_slice-1])
     samples_y.append(arr[tek_len+len_of_slice-1])
@@ -177,7 +176,6 @@ def slice(arr):
 
 for i in range(len(split_mod_descr)):
   slice(split_mod_descr[i])
-
 
 #Токенизация данных для обучения
 
@@ -213,44 +211,43 @@ result2 = res
 X = result1
 y = result2
 X_train, X_validation, y_train, y_validation = train_test_split(X, y, random_state=43, test_size=0.2)
-
-#EarlyStopping
+# EarlyStopping
 
 
 device = 'cpu'
 print(f"Using device: {device}")
 
+
 class EarlyStopping():
 
-  def __init__(self, patience=5, min_delta=1e-3, restore_best_weights=True):
-    self.patience = patience
-    self.min_delta = min_delta
-    self.restore_best_weights = restore_best_weights
-    self.best_model = None
-    self.best_loss = None
-    self.counter = 0
-    self.status = ""
+    def __init__(self, patience=5, min_delta=1e-3, restore_best_weights=True):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.restore_best_weights = restore_best_weights
+        self.best_model = None
+        self.best_loss = None
+        self.counter = 0
+        self.status = ""
 
+    def __call__(self, model, val_loss):
+        if self.best_loss == None:
+            self.best_loss = val_loss
+            self.best_model = copy.deepcopy(model)
+        elif self.best_loss - val_loss > self.min_delta:
+            self.best_loss = val_loss
+            self.counter = 0
+            self.best_model.load_state_dict(model.state_dict())
+        elif self.best_loss - val_loss < self.min_delta:
+            self.counter += 1
+            if self.counter >= self.patience:
+                self.status = f"Stopped on {self.counter}"
+                if self.restore_best_weights:
+                    model.load_state_dict(self.best_model.state_dict())
+                return True
 
-  def __call__(self, model, val_loss):
-    if self.best_loss == None:
-      self.best_loss = val_loss
-      self.best_model = copy.deepcopy(model)
-    elif self.best_loss - val_loss > self.min_delta:
-      self.best_loss = val_loss
-      self.counter = 0
-      self.best_model.load_state_dict(model.state_dict())
-    elif self.best_loss - val_loss < self.min_delta:
-      self.counter += 1
-      if self.counter >= self.patience:
-        self.status = f"Stopped on {self.counter}"
-        if self.restore_best_weights:
-          model.load_state_dict(self.best_model.state_dict())
-        return True
+        self.status = f"{self.counter}/{self.patience}"
+        return False
 
-
-    self.status = f"{self.counter}/{self.patience}"
-    return False
 
 #model
 
@@ -262,6 +259,7 @@ class Net(nn.Module):
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers)
+        self.lstm1 = nn.LSTM(input_size, hidden_size, num_layers)
         self.fc = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
@@ -269,6 +267,7 @@ class Net(nn.Module):
         h0 = torch.zeros(self.num_layers, self.hidden_size).to(x.device)
         c0 = torch.zeros(self.num_layers, self.hidden_size).to(x.device)
         out, (hn, cn) = self.lstm(x, (h0, c0))
+        out, (hn, cn) = self.lstm1(x, (hn, cn))
 
         return out, hn, cn
 
@@ -279,7 +278,7 @@ y_train = torch.Tensor(y_train).float()
 X_validation = torch.Tensor(X_validation).float().to(device)
 y_validation = torch.Tensor(y_validation).float().to(device)
 
-batch_size = 16
+batch_size = 4
 
 dataset_train = TensorDataset(X_train, y_train)
 dataloader_train = DataLoader(dataset_train,\
@@ -290,7 +289,7 @@ dataloader_test = DataLoader(dataset_test,\
   batch_size=batch_size)
 
 model = Net(X.shape[1], 4, 2, 1).to(device)
-
+model.load_state_dict(torch.load('lstm_model'))
 loss_fn = nn.MSELoss()
 
 optimizer = torch.optim.Adam(model.parameters(), lr = 0.01)
@@ -299,6 +298,7 @@ es = EarlyStopping()
 
 epoch = 0
 done = False
+'''
 while epoch < 100 and not done:
   epoch += 1
   steps = list(enumerate(dataloader_train))
@@ -320,7 +320,7 @@ while epoch < 100 and not done:
       pbar.set_description(f"Epoch: {epoch}, tloss: {loss}, vloss: {vloss:>7f}, EStop:[{es.status}]")
     else:
       pbar.set_description(f"Epoch: {epoch}, tloss {loss:}")
-
+'''
 def predict(model, input):
     model.eval()
     with torch.no_grad():
@@ -359,6 +359,7 @@ def preprocess_user_message(user_message):
 
 
 # функция добавления жанра в сообщение пользователя
+# функция добавления жанра в сообщение пользователя
 def add_predicted_genre(preprocess_user_message, model):
 
   # Преобразовать сообщение пользователя в формат, который принимает модель
@@ -376,7 +377,7 @@ def add_predicted_genre(preprocess_user_message, model):
 samples_df = [] #обрежем описания фильмов до размера 20
 
 def slice(arr):
-  len_of_slice = 20
+  len_of_slice = 30
   samples_df.append(arr[0:len_of_slice-1])
 
 for i in range(len(split_mod_descr)):
@@ -393,7 +394,6 @@ max_seq_length = max(len(seq) for seq in sequences)
 padded_sequences = pad_sequences(sequences, maxlen=max_seq_length, padding='post')
 
 result = (padded_sequences - padded_sequences.mean()) / padded_sequences.std()
-
 tensors_df = [] #векторные представления описаний фильмов
 
 
@@ -401,30 +401,34 @@ for i in range(len(result)):
   out, hidden_states, cell_states = predict(model, torch.Tensor(convert_to_2D(result[i])).float())
   tensors_df.append(hidden_states.numpy())
 
-#добавим индексацию для получения ответов
-new_df = df.assign(index=list(range(1, 1001)))
 
 def generate_movies(user_message):
-    
     preproc_user_message = preprocess_user_message(user_message)
 
     predicted_message = add_predicted_genre(preproc_user_message, model)
 
     tokenized_user_message = tokenizer.texts_to_sequences([predicted_message])
     padded_tokenized_user_message = pad_sequences(tokenized_user_message, maxlen=max_seq_length, padding='post')
-
+    padded_tokenized_user_message = (
+                                                padded_tokenized_user_message - padded_tokenized_user_message.mean()) / padded_tokenized_user_message.std()
     input_arr = convert_to_2D(padded_tokenized_user_message[0])
     out, hidden_states_user, cell_states = predict(model, torch.Tensor(input_arr).float())
 
     cos_dist = []
     ind = 1
+
     for i in tensors_df:
         cos_dist.append([ds.cosine(hidden_states_user.flatten(), i.flatten()), ind])
         ind += 1
+
     cos_dist.sort()
     top5 = cos_dist[:5]
-    top5_index = [i[1] for i in top5]
+    top5_index = [i[1] for i in top5]  # индексы топ5 фильмов
 
+    # добавим индексацию для получения ответов
+    new_df = df.assign(index=list(range(1, 1001)))
+
+    # топ5 названий
     res_name = new_df[new_df.index.isin(top5_index)]
     res_name = res_name['name'].tolist()
     return res_name
